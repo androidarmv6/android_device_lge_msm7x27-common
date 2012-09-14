4b923eae6b3485850dfd8fdc015966e1c0fcfdb2
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-09-14 18:58:31 -0300

    Removed crap

--- liblights/lights.c
+++ liblights/lights.c
@@ -48,32 +48,11 @@ static int g_haveAmberLed = 0;
 char const*const TRACKBALL_FILE
         = "/sys/class/leds/jogball-backlight/brightness";
 
-char const*const RED_LED_FILE
-        = "/sys/class/leds/red/brightness";
-
-char const*const GREEN_LED_FILE
-        = "/sys/class/leds/green/brightness";
-
 char const*const BLUE_LED_FILE
         = "/sys/class/leds/blue/brightness";
 
-char const*const AMBER_LED_FILE
-        = "/sys/class/leds/amber/brightness";
-
 char const*const LCD_FILE
         = "/sys/class/leds/lcd-backlight/brightness";
-
-char const*const RED_FREQ_FILE
-        = "/sys/class/leds/red/device/grpfreq";
-
-char const*const RED_PWM_FILE
-        = "/sys/class/leds/red/device/grppwm";
-
-char const*const RED_BLINK_FILE
-        = "/sys/class/leds/red/device/blink";
-
-char const*const AMBER_BLINK_FILE
-        = "/sys/class/leds/amber/blink";
 
 char const*const KEYBOARD_FILE
         = "/sys/class/leds/keyboard-backlight/brightness";
@@ -92,10 +71,6 @@ void init_globals(void)
 
     // figure out if we have the trackball LED or not
     g_haveTrackballLight = (access(TRACKBALL_FILE, W_OK) == 0) ? 1 : 0;
-
-    /* figure out if we have the amber LED or not.
-       If yes, just support green and amber.         */
-    g_haveAmberLed = (access(AMBER_LED_FILE, W_OK) == 0) ? 1 : 0;
 }
 
 static int
@@ -228,23 +203,7 @@ set_speaker_light_locked(struct light_device_t* dev,
     green = (colorRGB >> 8) & 0xFF;
     blue = colorRGB & 0xFF;
 
-    if (!g_haveAmberLed) {
-        write_int(RED_LED_FILE, red);
-        write_int(GREEN_LED_FILE, green);
-        write_int(BLUE_LED_FILE, blue);
-    } else {
-        /* all of related red led is replaced by amber */
-        if (red) {
-            write_int(AMBER_LED_FILE, 1);
-            write_int(GREEN_LED_FILE, 0);
-        } else if (green) {
-            write_int(AMBER_LED_FILE, 0);
-            write_int(GREEN_LED_FILE, 1);
-        } else {
-            write_int(GREEN_LED_FILE, 0);
-            write_int(AMBER_LED_FILE, 0);
-        }
-    }
+    write_int(BLUE_LED_FILE, blue);
 
     if (onMS > 0 && offMS > 0) {
         int totalMS = onMS + offMS;
@@ -266,16 +225,6 @@ set_speaker_light_locked(struct light_device_t* dev,
         blink = 0;
         freq = 0;
         pwm = 0;
-    }
-
-    if (!g_haveAmberLed) {
-        if (blink) {
-            write_int(RED_FREQ_FILE, freq);
-            write_int(RED_PWM_FILE, pwm);
-        }
-        write_int(RED_BLINK_FILE, blink);
-    } else {
-        write_int(AMBER_BLINK_FILE, blink);
     }
 
     return 0;
@@ -413,9 +362,9 @@ static struct hw_module_methods_t lights_module_methods = {
 struct hw_module_t HAL_MODULE_INFO_SYM = {
     .tag = HARDWARE_MODULE_TAG,
     .version_major = 1,
-    .version_minor = 0,
+    .version_minor = 1,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "QCT MSM7K lights Module",
-    .author = "Google, Inc.",
+    .name = "LG MSM7x27 Family Lights Module",
+    .author = "Rashed Abdel-Tawab",
     .methods = &lights_module_methods,
 };
