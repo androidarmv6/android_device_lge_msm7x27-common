2cf5f69ebcba3723eb6cb7e53b125d5bb8ab2fdb
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-10-07 16:56:29 -0400

    Add patches install script
    Just run this and it will install the patches. I'll upload an uninstall script soon too

---
+++ patches/Install.sh
@@ -0,0 +1,11 @@
+cd /Volumes/Android/CM10/device/lge/msm7x27-common/patches
+patch -p1 < frameworks_base/0001-Triggering-EarlySuspend-based-on-BackLight-brightnes.patch
+patch -p1 < frameworks_base/0002-Fix-to-manually-search-networks.patch
+patch -p1 < frameworks_base/0001-Triggering-EarlySuspend-based-on-BackLight-brightnes.patch
+patch -p1 < packages_apps_LegacyCamera/0001-load-correct-library-for-panorama.patch
+patch -p1 < packages_apps_LegacyCamera/0002-mosaic-Hack-renderer-to-support-devices-without-exte.patch
+patch -p1 < packages_apps_LegacyCamera/0003-Add-support-for-Panorama-mode.patch
+patch -p1 < packages_apps_Gallery2/0001-hide-jelly-bean-camera.patch
+patch -p1 < external_libncurses/0001-Revert-Adding-code-to-copy-terminfo-data-to-system-e.patch
+patch -p1 < external_webkit/0001-Hack-shaders-to-work-on-devices-without-OES_external.patch
+cd /Volumes/Android/CM10
