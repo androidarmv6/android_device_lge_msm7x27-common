3bc891db9d5e9d291438f754f4441886b366a0dc
Giorgio Andronico <techdevelop.x@gmail.com>
2012-10-10 00:22:00 +0300

    Uninstall.sh: Typo

--- patches/Uninstall.sh
+++ patches/Uninstall.sh
@@ -4,26 +4,26 @@
 echo "Obtaining build directory..."
 rootdirectory="$PWD"
 cd frameworks/base
-echo "Applying frameworks/base patches..."
+echo "Reversing frameworks/base patches..."
 patch -R < $rootdirectory/device/lge/msm7x27-common/patches/frameworks_base/0001-Triggering-EarlySuspend-based-on-BackLight-brightnes.patch
 patch -R < $rootdirectory/device/lge/msm7x27-common/patches/frameworks_base/0002-Fix-to-manually-search-networks.patch
 cd $rootdirectory
 cd packages/apps/LegacyCamera
-echo "Applying LegacyCamera patches..."
+echo "Reversing LegacyCamera patches..."
 patch -R < $rootdirectory/device/lge/msm7x27-common/patches/packages_apps_LegacyCamera/0001-load-correct-library-panorama.patch
 patch -R < $rootdirectory/device/lge/msm7x27-common/patches/packages_apps_LegacyCamera/0002-mosaic-Hack-renderer-to-support-devices-without-exte.patch
 patch -R < $rootdirectory/device/lge/msm7x27-common/patches/packages_apps_LegacyCamera/0003-Add-panorama-mode-support.patch
 cd $rootdirectory
 cd packages/apps/Gallery2
-echo "Applying Gallery2 patches..."
+echo "Reversing Gallery2 patches..."
 patch -R < $rootdirectory/device/lge/msm7x27-common/patches/packages_apps_Gallery2/0001-hide-jelly-bean-camera.patch
 cd $rootdirectory
 cd external/libncurses
-echo "Applying libncurses patches..."
+echo "Reversing libncurses patches..."
 patch -R < $rootdirectory/device/lge/msm7x27-common/patches/external_libncurses/0001-Revert-Adding-code-to-copy-terminfo-data-to-system-e.patch
 cd $rootdirectory
 cd external/webkit
-echo "Applying webkit patches..."
+echo "Reversing webkit patches..."
 patch -R < $rootdirectory/device/lge/msm7x27-common/patches/external_webkit/0001-Hack-shaders-to-work-on-devices-without-OES_external.patch
 echo "Changing to build directory.."
 cd $rootdirectory
