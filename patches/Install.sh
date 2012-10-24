44e60a5e629b1e020f34a6b9e5584ff038bd49c7
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-10-24 09:53:46 -0300

    Force patches to install to new branch
    This makes 2 branches in each of the project. One that isn't patched,
    and one that is. It will automatically move you over to the patched
    branch once it is finished. Also fixed a tiny error with frameworks/av
    and frameworks/native

--- patches/Install.sh
+++ patches/Install.sh
@@ -1,12 +1,28 @@
 # This script installs automatically all needed patches for the devices supported by lgics. #
-# Concept by Rashed97,reviewed by Bytecode #
+# Concept and enhancements by Rashed97, reviewed by Bytecode #
 
 echo "Obtaining build directory..."
 rootdirectory="$PWD"
+repo start non-patched frameworks/base
+repo start patched frameworks/base
 cd frameworks/base
 echo "Applying frameworks/base patches..."
 git am $rootdirectory/device/lge/msm7x27-common/patches/frameworks_base/*.patch
 cd $rootdirectory
+repo start non-patched frameworks/av
+repo start patched frameworks/av
+cd frameworks/av
+echo "Applying frameworks/av patches..."
+git pull http://review.cyanogenmod.com/CyanogenMod/android_frameworks_av refs/changes/03/23603/1
+cd $rootdirectory
+repo start non-patched frameworks/native
+repo start patched frameworks/native
+cd frameworks/native
+echo "Applying frameworks/native patches..."
+git pull http://review.cyanogenmod.com/CyanogenMod/android_frameworks_native refs/changes/02/23602/1
+cd $rootdirectory
+repo start non-patched packages/apps/LegacyCamera
+repo start patched packages/apps/LegacyCamera
 cd packages/apps/LegacyCamera
 echo "Applying LegacyCamera patches..."
 git pull http://review.cyanogenmod.com/CyanogenMod/android_packages_apps_LegacyCamera refs/changes/79/24979/2
@@ -14,28 +30,28 @@ git pull http://review.cyanogenmod.com/CyanogenMod/android_packages_apps_LegacyC
 git pull http://review.cyanogenmod.com/CyanogenMod/android_packages_apps_LegacyCamera refs/changes/81/24981/1
 git pull http://review.cyanogenmod.com/CyanogenMod/android_packages_apps_LegacyCamera refs/changes/13/25113/2
 cd $rootdirectory
+repo start non-patched packages/apps/Gallery2
+repo start patched packages/apps/Gallery2
 cd packages/apps/Gallery2
 echo "Applying Gallery2 patches..."
 git pull http://review.cyanogenmod.com/CyanogenMod/android_packages_apps_Gallery2 refs/changes/15/25115/3
 cd $rootdirectory
+repo start non-patched packages/apps/Settings
+repo start patched packages/apps/Settings
 cd packages/apps/Settings
 echo "Applying Settings patches..."
 git am $rootdirectory/device/lge/msm7x27-common/patches/packages_apps_Settings/0001-Add-forum-link-to-device-info.patch
 cd $rootdirectory
+repo start non-patched external/libncurses
+repo start patched external/libncurses
 cd external/libncurses
 echo "Applying libncurses patches..."
 git am $rootdirectory/device/lge/msm7x27-common/patches/external_libncurses/0001-Revert-Adding-code-to-copy-terminfo-data-to-system-e.patch
 cd $rootdirectory
+repo start non-patched external/webkit
+repo start patched external/webkit
 cd external/webkit
 echo "Applying webkit patches..."
 git am $rootdirectory/device/lge/msm7x27-common/patches/external_webkit/0001-Hack-shaders-to-work-on-devices-without-OES_external.patch
-cd $rootdirectory
-cd frameworks_av
-echo "Applying frameworks/av patches..."
-git pull http://review.cyanogenmod.com/CyanogenMod/android_frameworks_av refs/changes/03/23603/1
-cd $rootdirectory
-cd frameworks_native
-echo "Applying frameworks/native patches..."
-git pull http://review.cyanogenmod.com/CyanogenMod/android_frameworks_native refs/changes/02/23602/1
-echo "Changing to build directory.."
+echo "Changing to build directory..."
 cd $rootdirectory
