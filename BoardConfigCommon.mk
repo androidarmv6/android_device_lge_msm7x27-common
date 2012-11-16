4f825761581380e533bb555204f2fbc80cdeed2a
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-11-16 08:36:25 -0500

    Cleanup and reorganize

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -3,7 +3,7 @@
 # against the traditional rules of inheritance).
 USE_CAMERA_STUB := true
 
-# Camera
+## Camera
 TARGET_DISABLE_ARM_PIE := true
 BOARD_NEEDS_MEMORYHEAPPMEM := true
 BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
@@ -13,13 +13,13 @@ COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_COMPAT
 # BOARD_USE_CAF_LIBCAMERA := true
 BOARD_USES_LEGACY_CAMERA := true
 
-# Kernel
+## Kernel
 TARGET_KERNEL_SOURCE := kernel/lge/msm7x27
 # Copy LG Kernel Headers here if necessary, DON'T use Adroid auto-generated headers
 TARGET_SPECIFIC_HEADER_PATH := device/lge/msm7x27-common/include
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=qcom no_console_suspend
 
-# CPU & Platform
+## CPU & Platform
 ARCH_ARM_HAVE_VFP := true
 TARGET_CPU_ABI := armeabi-v6l
 TARGET_CPU_ABI2 := armeabi
@@ -28,7 +28,7 @@ TARGET_BOARD_PLATFORM := msm7x27
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 COMMON_GLOBAL_CFLAGS += -DTARGET_MSM7x27
 
-# Browser & WebKit
+## Browser & WebKit
 JS_ENGINE := v8
 HTTP := chrome
 WITH_JIT := true
@@ -36,7 +36,7 @@ ENABLE_JSC_JIT := true
 ENABLE_WEBGL := true
 TARGET_FORCE_CPU_UPLOAD := true
 
-# Boot loader & recovery
+## Boot loader & recovery
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_USE_RGB565 := true
@@ -44,6 +44,7 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 BOARD_HAS_NO_SELECT_BUTTON := true
 
+## Partition Sizes
 # Fix this up by examining /proc/mtd on a running device
 BOARD_KERNEL_BASE := 0x12800000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -57,35 +58,42 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0xE600000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0c780000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
+## Graphics
 # Enable OpenGL Hardware Acceleration
 USE_OPENGL_RENDERER := true
+# Force non usage of VSync
 TARGET_NO_HW_VSYNC := true
 TARGET_USES_GENLOCK := true
 TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
-BOARD_USES_QCOM_HARDWARE := true
-BOARD_USES_QCOM_LIBS := true
-BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
 BOARD_USE_SKIA_LCDTEXT := true
 BOARD_EGL_CFG := device/lge/msm7x27-common/configs/egl.cfg
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+# Framerate
 COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
 COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK
+# Legacy Graphics CFLAGS
 COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_PIXEL_FORMAT_YV12
 COMMON_GLOBAL_CFLAGS += -DMISSING_GRALLOC_BUFFERS
 COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_EXTERNAL_IMAGE
+
+## Qualcomm and Adreno Hardware
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
 # Add support for OMX
 COMMON_GLOBAL_CFLAGS += -DQCOM_LEGACY_OMX
 
+## GPS
 # Enable the GPS HAL & AMSS version to use for GPS
 BOARD_USES_QCOM_LIBRPC := true
 BOARD_USES_QCOM_GPS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
-# Use the custom lights libs
+## Lights
 TARGET_PROVIDES_LIBLIGHTS := true
 
-# Audio, Bluetooth & FM Radio
+## Audio, Bluetooth & FM Radio
 TARGET_PROVIDES_LIBAUDIO := true
 BOARD_USES_ICS_LIBAUDIO := true
 COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB
@@ -101,17 +109,17 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 # COMMON_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
 # COMMON_GLOBAL_CFLAGS += -DQCOM_FM_ENABLED
 
-# RIL
+## RIL
 BOARD_PROVIDES_LIBRIL := true
 
-# Mass Storage for Jelly Bean
+## Mass Storage for Jelly Bean
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun/file
 
-# Touch screen compatibility for Jelly Bean
+## Touch screen compatibility for Jelly Bean
 BOARD_USE_LEGACY_TOUCHSCREEN := true
 
-# Wi-Fi & Wi-Fi HotSpot
+## Wi-Fi & Wi-Fi HotSpot
 WPA_SUPPLICANT_VERSION          := VER_0_6_X
 BOARD_WLAN_DEVICE               := bcm4325
 BOARD_WEXT_NO_COMBO_SCAN        := true
@@ -123,5 +131,5 @@ WIFI_DRIVER_MODULE_NAME         := "wireless"
 WIFI_DRIVER_FW_PATH_STA         := "/system/etc/wl/rtecdc.bin"
 WIFI_DRIVER_FW_PATH_AP          := "/system/etc/wl/rtecdc-apsta.bin"
 
-# OTA script
+## OTA script
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/lge/msm7x27-common/releasetools/ota_from_target_files
