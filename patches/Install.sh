f0acf0d8ae78c5e5a945ba755e604003962c47f6
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-12-04 19:52:38 -0500

    Update patches
    Add OMX patches, build patch, and OMX building patch

--- patches/Install.sh
+++ patches/Install.sh
@@ -7,7 +7,7 @@ repo start non-patched build
 repo start patched build
 cd build
 echo "Applying build patches..."
-git pull http://github.com/androidarmv6/android_build jellybean
+git am $rootdirectory/device/lge/msm7x27-common/patches/build/*.patch
 cd $rootdirectory
 repo start non-patched frameworks/base
 repo start patched frameworks/base
@@ -19,19 +19,20 @@ repo start non-patched frameworks/av
 repo start patched frameworks/av
 cd frameworks/av
 echo "Applying frameworks/av patches..."
-git pull http://github.com/androidarmv6/android_frameworks_av jellybean
+git am $rootdirectory/device/lge/msm7x27-common/patches/frameworks/av/*.patch
 cd $rootdirectory
 repo start non-patched frameworks/native
 repo start patched frameworks/native
 cd frameworks/native
 echo "Applying frameworks/native patches..."
-git pull http://github.com/androidarmv6/android_frameworks_native jellybean
+git pull http://Rashed@review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/02/23602/1
+git pull http://Rashed@review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/26/26526/1
 cd $rootdirectory
 repo start non-patched hardware/qcom/media
 repo start patched hardware/qcom/media
 cd hardware/qcom/media
 echo "Applying hardware/qcom/media patches..."
-git pull http://github.com/androidarmv6/android_hardware_qcom_media jellybean
+git am $rootdirectory/device/lge/msm7x27-common/patches/hardware/qcom/media/*.patch
 cd $rootdirectory
 repo start non-patched packages/apps/Settings
 repo start patched packages/apps/Settings
