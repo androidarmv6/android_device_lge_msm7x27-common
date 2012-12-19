046bdfbaa144997e15ece96674b42716b2f11c11
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2012-12-19 23:46:39 +0400

    Fix conflict with samsung msm7x27
    Change-Id: Ibc786a92fbddc51a7295de8959b665471315735e

--- Android.mk
+++ Android.mk
@@ -1,3 +1,7 @@
+LGE_TARGETS := p500 e510 p509 p506 p505 p350
+
+ifneq ($(filter $(LGE_TARGETS),$(TARGET_DEVICE)),)
+
 # HACK for prebuilt libril and libcamera
 $(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libril_intermediates)
 $(shell touch $(OUT)/obj/SHARED_LIBRARIES/libril_intermediates/export_includes)
@@ -6,6 +10,5 @@ $(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_include
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x27)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
