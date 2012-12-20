13eda1c7e4e24081a87e5af7bc6f0d312903383b
Rashed  Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-12-20 17:12:25 -0500

    Update headers
    Update headers to match kernel headers
    
    Change-Id: I3e5b9f541797c621e01fbb3dbb552d3ee413686a

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002,2007-2011, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2002,2007-2010, Code Aurora Forum. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -490,3 +490,4 @@ int kgsl_gem_obj_addr(int drm_fd, int handle, unsigned long *start,
 #endif
 #endif
 #endif /* _MSM_KGSL_H */
+
