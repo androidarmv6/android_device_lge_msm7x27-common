75a5c9420332f13ac724f01443a1643c36238afb
GenetICS <gntcs@msn.com>
2012-12-20 11:38:06 +0200

    Build LGE targets

--- Android.mk
+++ Android.mk
@@ -1,14 +1,11 @@
-LGE_TARGETS := p500 e510 p509 p506 p505 p350
+ifeq (LGE,$(PRODUCT_MANUFACTURER))
+  ifeq (msm7x27,$(TARGET_BOARD_PLATFORM))
+    # HACK for prebuilt libril and libcamera
+    $(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libril_intermediates)
+    $(shell touch $(OUT)/obj/SHARED_LIBRARIES/libril_intermediates/export_includes)
+    $(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates)
+    $(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes)
 
-ifneq ($(filter $(LGE_TARGETS),$(TARGET_DEVICE)),)
-
-# HACK for prebuilt libril and libcamera
-$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libril_intermediates)
-$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libril_intermediates/export_includes)
-$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates)
-$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes)
-
-LOCAL_PATH := $(call my-dir)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
+    include $(call all-makefiles-under,$(call my-dir))
+  endif
 endif
