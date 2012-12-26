10d9fe6c661a2dd257515416b7dc72692e728262
AndroidMeda <hephappy@gmail.com>
2012-12-26 18:58:55 +0200

    Gerrit test
    Change-Id: I2dbfb0483865977477baf4858c7fc33b61a37a42

--- extract-files.sh
+++ extract-files.sh
@@ -5,7 +5,7 @@ DEVICE=msm7x27-common
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 
-echo "Pulling common files..."
+echo "Pulling common files...!"
 for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
