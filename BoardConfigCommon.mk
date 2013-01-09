0b28c0684f016c14ee5917f016fc1c2b57541c26
AndroidMeda <hephappy@gmail.com>
2013-01-09 11:45:29 +0200

    lge/msm7x27-common: Don't remove core fonts, use SMALLER_FONT_FOOTPRINT instead
    Change-Id: I9644b7e28c917f7814ad4d320b8929b9733d3d18

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -17,37 +17,25 @@
 # Use the Qualcomm common folder
 include device/qcom/msm7x27/BoardConfigCommon.mk
 
-## Camera
-BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
-COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
-#BOARD_USES_LEGACY_CAMERA := true
-#BOARD_CPU_COLOR_CONVERT := true
-
-## Kernel
-TARGET_KERNEL_SOURCE := kernel/lge/msm7x27
-# Copy LG Kernel Headers here if necessary, DO NOT use Android auto-generated headers
-TARGET_SPECIFIC_HEADER_PATH := device/lge/msm7x27-common/include
-BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=qcom
-
-## Browser & WebKit
-ENABLE_WEBGL := true
-
 ## Boot loader & recovery
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+BOARD_HAS_NO_SELECT_BUTTON := true
 
-## Partition Sizes
-# Fix this up by examining /proc/mtd on a running device
+## Kernel
+TARGET_KERNEL_SOURCE := kernel/lge/msm7x27
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom
 BOARD_KERNEL_BASE := 0x12800000
 BOARD_KERNEL_PAGESIZE := 2048
+
+## *** Copy LG Kernel Headers here if necessary, DO NOT use Android auto-generated headers ***
+TARGET_SPECIFIC_HEADER_PATH := device/lge/msm7x27-common/include
+
+## Partition Sizes: Fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00440000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
-# Ignore that this is much larger than the 190Mb allowed.
-# It's only for resolving build issues with the system.img
-# being too big. It will still be only about 100MB MAXIMUM
-# because all the useless trash is now in extras
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0xE600000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0xff00000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0c780000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
@@ -56,39 +44,18 @@ BOARD_EGL_NEEDS_LEGACY_FB := true
 TARGET_DISABLE_TRIPLE_BUFFERING := true
 COMMON_GLOBAL_CFLAGS += -DANCIENT_GL
 
-## GPS
-# Enable the GPS HAL & AMSS version to use for GPS
-BOARD_USES_QCOM_LIBRPC := true
-BOARD_USES_QCOM_GPS := true
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
-BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
-
-## Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
-## Bluetooth
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUEDROID_VENDOR_CONF := device/lge/msm7x27-common/bluetooth/libbt_lge.txt
+## Camera
+BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
+#BOARD_USES_LEGACY_CAMERA := true
+#BOARD_CPU_COLOR_CONVERT := true
 
 ## Audio & FM Radio
 BOARD_COMBO_DEVICE_SUPPORTED := true
 # BOARD_FM_DEVICE := bcm4325
 # BOARD_HAVE_FM_RADIO := true
-# Use the second one (QCOM_FM_ENABLED) not the first
-# The first breaks the builds and the second is more
-# recent and is used in the CM code
 # COMMON_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
 # COMMON_GLOBAL_CFLAGS += -DQCOM_FM_ENABLED
-
-## RIL
-BOARD_PROVIDES_LIBRIL := true
-
-## Mass Storage for Jelly Bean
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
-BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun/file
-
-## Touch screen compatibility for Jelly Bean
-BOARD_USE_LEGACY_TOUCHSCREEN := true
 
 ## Wi-Fi & Wi-Fi HotSpot
 WPA_SUPPLICANT_VERSION          := VER_0_8_X
@@ -103,5 +70,34 @@ WIFI_DRIVER_MODULE_NAME         := "wireless"
 WIFI_DRIVER_FW_PATH_STA         := "/system/etc/wl/rtecdc.bin"
 WIFI_DRIVER_FW_PATH_AP          := "/system/etc/wl/rtecdc-apsta.bin"
 
-## OTA script extra files (build/tools/releasetools)
+## Bluetooth
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/lge/msm7x27-common/bluetooth/libbt_lge.txt
+
+## GPS
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+## RIL
+BOARD_PROVIDES_LIBRIL := true
+
+## Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+## Mass Storage
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun/file
+
+## Touch screen compatibility
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+## Minimal fonts
+SMALLER_FONT_FOOTPRINT := true
+
+## Browser & WebKit
+ENABLE_WEBGL := true
+
+## OTA script extras file (build/tools/releasetools)
 TARGET_OTA_EXTRAS_FILE := device/lge/msm7x27-common/releasetools/extras.txt
