087d51ac1fe2c26331b7053799bab98349b91ce0
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2013-02-02 20:51:04 -0500

    Implement libbt-vendor for LG devices
    Update the bluetooth lib to standard 4.2 style
    
    Change-Id: I1a419836071e3195c65e973dd49319e25adcca7c

--- bluetooth/init.qcom.bt.sh
+++ bluetooth/init.qcom.bt.sh
@@ -4,8 +4,6 @@ BLUETOOTH_SLEEP_PATH=/proc/bluetooth/sleep/proto
 LOG_TAG="qcom-bluetooth"
 LOG_NAME="${0}:"
 
-hciattach_pid=""
-
 loge ()
 {
   /system/bin/log -t $LOG_TAG -p e "$LOG_NAME $@"
@@ -19,25 +17,13 @@ logi ()
 failed ()
 {
   loge "$1: exit code $2"
+  setprop bluetooth.status off;
   exit $2
 }
 
-start_hciattach ()
-{
-  echo 1 > $BLUETOOTH_SLEEP_PATH
-  /system/bin/hciattach -n /dev/ttyHS0 any 3000000 flow &
-  hciattach_pid=$!
-  logi "start_hciattach: pid = $hciattach_pid"
-}
+setprop bluetooth.status off
 
-kill_hciattach ()
-{
-  logi "kill_hciattach: pid = $hciattach_pid"
-  ## careful not to kill zero or null!
-  kill -TERM $hciattach_pid
-  echo 0 > $BLUETOOTH_SLEEP_PATH
-  # this shell doesn't exit now -- wait returns for normal exit
-}
+# echo 1 > $BLUETOOTH_SLEEP_PATH
 
 /system/bin/brcm_patchram_plus -d --patchram /etc/firmware/BCM4325D1_004.002.004.0218.0248.hcd /dev/ttyHS0
 logi "Setting baudrate..."
@@ -49,20 +35,8 @@ case $exit_code_hci_qcomm_download in
   *) failed "Bluetooth QSoC firmware download failed" $exit_code_hci_qcomm_download;;
 esac
 
-# init does SIGTERM on ctl.stop for service
-trap "kill_hciattach" TERM INT
+setprop bluetooth.status on
 
-start_hciattach
-
-# Give hciattach time to settle
-sleep 10
-logi "Fixing PCM setting..."
-/system/xbin/hcitool cmd 0x3f 0x1c 0x00 0x04 0x00 0x00 0x00 || loge "FAILED"
-logi "Writing Sleep mode setting..."
-/system/xbin/hcitool cmd 0x3f 0x27 0x01 0x01 0x10 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 || loge "FAILED"
-
-wait $hciattach_pid
-
-logi "Bluetooth stopped"
+logi "start bluetooth transport"
 
 exit 0
