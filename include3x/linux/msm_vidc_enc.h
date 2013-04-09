472f4bd7d22940a30b3c8f5146f4fe44341753ea
AndroidMeda <hephappy@gmail.com>
2013-04-09 15:02:44 +0300

    Add jb_mr1_chocolate(3.0.31) headers
    Change-Id: I6e26b2b085260a8331b818296daa3d1c382494bc

--- include3x/linux/msm_vidc_enc.h
+++ include3x/linux/msm_vidc_enc.h
@@ -454,6 +454,14 @@ struct venc_ioctl_msg{
 #define VEN_IOCTL_GET_EXTRADATA \
 	_IOR(VEN_IOCTLBASE_ENC, 49, struct venc_ioctl_msg)
 
+/*IOCTL params:SET: InputData - NULL, OutputData - NULL.*/
+#define VEN_IOCTL_SET_SLICE_DELIVERY_MODE \
+	_IO(VEN_IOCTLBASE_ENC, 50)
+
+/*IOCTL params:SET: InputData - unsigned int, OutputData - NULL*/
+#define VEN_IOCTL_SET_SPS_PPS_FOR_IDR \
+	_IOW(VEN_IOCTLBASE_ENC, 51, struct venc_ioctl_msg)
+
 struct venc_switch{
 	unsigned char	status;
 };
