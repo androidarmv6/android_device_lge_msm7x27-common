7709d9cc73d34557a76b6e83b77b7066cd43ba7a
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2013-08-06 17:43:38 -0400

    Inherit MDPI common initlogo
    Add a new AndroidARMv6 initlogo
    I've had this for ages now, never got around to adding
    it though.
    
    Change-Id: I76747d5bb5a9a119e7cc159042f122b38b7e0c6c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,19 +1,14 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+# Use the MDPI common folder
+include device/mdpi-common/etc/Android.mk
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := initlogo.rle
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/initlogo.rle
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
