9a76d876fc7b973ee80746834ae46ae9ae6ab653
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2013-08-19 00:39:40 -0400

    Update SE Policy (WIP)
    -Added a few lines in file_contexts
    -Added a few files
    
    Change-Id: I51dac2ae59c4f9ed9be9b652d02c76212413d137

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,11 +41,19 @@ BOARD_KERNEL_BASE := 0x12800000
 BOARD_KERNEL_PAGESIZE := 2048
 
 ## SELinux
-BOARD_SEPOLICY_DIRS += device/lge/msm7x27-common/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/lge/msm7x27-common/sepolicy
+
 BOARD_SEPOLICY_UNION += \
-    genfs_contexts \
+    dhcp.te \
+    domain.te \
     file_contexts \
-    sepolicy.te
+    init.te \
+    mediaserver.te \
+    surfaceflinger.te \
+    sepolicy.te \
+    ueventd.te \
+    wpa_supplicant.te
 
 ## Partition Sizes: Fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00440000
