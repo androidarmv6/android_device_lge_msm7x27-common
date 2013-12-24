29d2130713afb32289234f94d4487bde07e1d68a
Conn O'Griofa <connogriofa@gmail.com>
2013-12-24 21:54:30 +0200

    Add basic SELinux policy for /sd-ext
    * Add unionized file_contexts to extend /sd-ext support. This fixes
      /sd-ext formatting in recovery, and adds potential support for
      backup & restore of user data.
    * Set SELinux to permissive mode by default. We need to add more rules
      in order for enforcing mode to work correctly.
    
    Change-Id: I700c89640309b2d02bfcde6d0598afda6e667da9

--- device.mk
+++ device.mk
@@ -130,12 +130,13 @@ PRODUCT_PACKAGES += \
     make_ext4fs \
     setup_fs
 
-# Use set_perm (no selinux version) instead of set_metadata (selinux version)
-#USE_SET_METADATA := false
-
-# KitKat uses selinux enforcing mode by default
+# SELinux - we're not ready for enforcing mode yet
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.selinux=permissive
+
+BOARD_SEPOLICY_DIRS += device/lge/msm7x27-common/sepolicy
+BOARD_SEPOLICY_UNION += \
+    file_contexts
 
 # Offmode charging
 #PRODUCT_PACKAGES += \
