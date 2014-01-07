e7e0cda72e635cc63d1bcfb5c9ff1607a0ca722c
Conn O'Griofa <connogriofa@gmail.com>
2014-01-07 17:24:45 +0200

    Update camera HAL
    * camera HAL adapted from Evervolv:
    https://github.com/Evervolv/android_hardware_qcom_camera/tree/jellybean-4.1.2
    
    Notes:
    * Requires Froyo liboemcamera library to function properly.
    * Implements TARGET_CAMERA_SENSOR_SIZE to specify default preview
      size. Possible combinations are 5MP, 3MP and 2MP. Defaults to 2MP.
    
    Change-Id: If8cd70a048fcc2cbbc2092e6c1df3537563de8ae

--- device.mk
+++ device.mk
@@ -92,7 +92,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.gr.numframebuffers=3 \
     persist.sys.purgeable_assets=1 \
     persist.service.adb.enable=1
-    
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     initlogo.rle \
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
     com.android.future.usb.accessory \
     lights.msm7x27 \
     lgapversion
-    
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.default \
@@ -127,7 +127,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libcamera \
     camera.msm7x27
 
 # Filesystem management tools
