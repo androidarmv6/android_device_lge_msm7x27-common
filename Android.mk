a8a3415ba95378ad37d7994ddd28184060417df7
androidmeda <hephappy@gmail.com>
2014-01-15 11:46:36 +0200

    Provide prebuilt froyo libcamera.so (TARGET_PREBUILT_LIBCAMERA)
    Change-Id: Ib88b70d6b60a70e21e2207835d2372eef01c58dc

--- Android.mk
+++ Android.mk
@@ -1,5 +1,10 @@
 ifeq (LGE,$(PRODUCT_MANUFACTURER))
-  ifeq (msm7x27,$(TARGET_BOARD_PLATFORM))
-    include $(call all-makefiles-under,$(call my-dir))
-  endif
+    ifeq (msm7x27,$(TARGET_BOARD_PLATFORM))
+        # HACK for prebuilt libcamera
+        ifeq ($(TARGET_PREBUILT_LIBCAMERA),true)
+            $(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates)
+            $(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes)
+        endif
+	include $(call all-makefiles-under,$(call my-dir))
+    endif
 endif
