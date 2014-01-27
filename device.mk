e0e2cc819fc442aa352f09e3f26829944293088e
androidmeda <hephappy@gmail.com>
2014-01-27 15:00:58 +0200

    Remove bluez support (1/2)
    Change-Id: I7c1e8e54a4ef8dbd7bb479013b000747679de26e

--- device.mk
+++ device.mk
@@ -1,48 +1,9 @@
 # Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-## BlueZ support
-## Note: needs to be defined here in order to satisfy inheritance issues.
-## If disabled, Bluedroid will be used.
-#BOARD_HAVE_BLUETOOTH_BLUEZ := true
-
-ifdef BOARD_HAVE_BLUETOOTH_BLUEZ
-# BlueZ: binaries
-PRODUCT_PACKAGES += \
-    bluetoothd \
-    brcm_patchram_plus \
-    libbluetoothd \
-    hcitool \
-    hciconfig \
-    hciattach
-
-# BlueZ: configs
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/audio.conf:system/etc/bluetooth/audio.conf \
-    system/bluetooth/data/auto_pairing.conf:system/etc/bluetooth/auto_pairing.conf \
-    system/bluetooth/data/blacklist.conf:system/etc/bluetooth/blacklist.conf \
-    system/bluetooth/data/input.conf:system/etc/bluetooth/input.conf \
-    system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf \
-    system/bluetooth/data/network.conf:system/etc/bluetooth/network.conf
-
-# BlueZ: javax.btobex is required by Bluetooth_msm
-PRODUCT_PACKAGES += \
-    javax.btobex
-
-# BlueZ: rc
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bluetooth/init.qcom.bluez.rc:root/init.qcom.bluetooth.rc
-
-else
-
 # Bluedroid: rc
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/bluetooth/init.qcom.bluedroid.rc:root/init.qcom.bluetooth.rc
-
-endif #BOARD_HAVE_BLUETOOTH_BLUEZ
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bluetooth/init.qcom.bt.sh:system/bin/init.qcom.bt.sh
 
 PRODUCT_PACKAGES += \
     hwaddrs
@@ -138,7 +99,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 BOARD_SEPOLICY_DIRS += device/lge/msm7x27-common/sepolicy
 BOARD_SEPOLICY_UNION += \
-    file_contexts
+    file_contexts \
+    genfs_contexts
 
 # Offmode charging
 #PRODUCT_PACKAGES += \
