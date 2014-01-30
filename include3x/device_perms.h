3eb9e7e045063c75a0e77fc242e3da7fbf35d6a9
androidmeda <hephappy@gmail.com>
2014-01-30 11:48:12 +0200

    Allow get/set service.brcm.bt.* property to radio user
    Fixes BT MAC address
    
    Change-Id: I5884f45e2b494c7cab4d90d4b47f169d67923666

---
+++ include3x/device_perms.h
@@ -0,0 +1,7 @@
+#define PROPERTY_PERMS_APPEND \
+    { "service.brcm.bt.",      AID_RADIO,    0 },
+
+//#define CONTROL_PERMS_APPEND \
+//    { "aaa", AID_RADIO, AID_RADIO }, \
+//    { "bbb", AID_RADIO, AID_RADIO},
+
