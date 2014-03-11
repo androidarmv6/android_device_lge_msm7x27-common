1df2811e84ea80932430e769afebdca7ee4e8483
Hard Light <hardlightxda@gmail.com>
2014-03-11 15:28:08 +0200

    Another try to fix TWRP brightness
    TWRP cannot read brightness file, because of quotes:
    
    I:Cannot find file "/sys/class/leds/lcd-backlight/brightness"
    
    Twrp already had been added quotes:
    https://github.com/omnirom/android_bootable_recovery/blob/android-4.4/minui/Android.mk#L35
    
    Change-Id: If3a0aecc7558a2aae2df4f42974319483f72ed1b

--- BoardConfigRecovery.mk
+++ BoardConfigRecovery.mk
@@ -5,7 +5,7 @@ TARGET_NO_INITLOGO := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 
-TW_BRIGHTNESS_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_EXCLUDE_SUPERSU := true
 TW_EXCLUDE_ENCRYPTED_BACKUPS := true
 TW_MAX_BRIGHTNESS := 255
