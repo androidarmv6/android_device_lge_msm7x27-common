d1a9f761d5182ab1ef7fae4e6893872d0bdfca14
androidmeda <hephappy@gmail.com>
2014-07-02 08:00:47 +0300

    Remove custom recovery_keys.c
    recovery : SEARCH should work as SELECT_ITEM
    https://github.com/androidarmv6/android_bootable_recovery/commit/5ee09a20c8c8c4cd1841f182d7b4c326efc32f1f
    
    Change-Id: Ida78154f8b2cf4682565a80ee05ea60c769f989e

--- BoardConfigRecovery.mk
+++ BoardConfigRecovery.mk
@@ -6,7 +6,6 @@ TARGET_NO_INITLOGO := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/lge/msm7x27-common/recovery/recovery_keys.c
 
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_EXCLUDE_SUPERSU := true
