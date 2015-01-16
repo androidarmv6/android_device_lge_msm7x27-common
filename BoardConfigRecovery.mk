1530bd3b1bc9bae595525a9f664e62e124a4cfa4
AndroidMeda <hephappy@gmail.com>
2015-01-16 16:28:29 +0200

    Updates for common device
    - Enable dex-preoptimization to speed up first boot sequence
    - Increase data and system partition (512MB)
    - Seperate twrp and cm recovery
    - Define TARGET_RECOVERY_DEVICE_DIRS to keep twrp.fstab in recovery
    - build: Avoid poisoning the host binaries with bad ABIs
    - Disable TARGET_OTA_EXTRAS_FILE
    
    Change-Id: I200c2ea2fab0f7bc1f50935883229179df02f681

--- BoardConfigRecovery.mk
+++ BoardConfigRecovery.mk
@@ -2,15 +2,20 @@ DEVICE_RESOLUTION := 320x480
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"font_7x16.h\"
 TARGET_NO_SEPARATE_RECOVERY := true
-
 TARGET_NO_INITLOGO := true
-TARGET_RECOVERY_FSTAB := device/lge/msm7x27-common/recovery/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
-TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 
+TARGET_RECOVERY_FSTAB := device/lge/msm7x27-common/recovery/root/etc/recovery.fstab
+TARGET_RECOVERY_DEVICE_DIRS += device/lge/msm7x27-common
 
+ifeq ($(RECOVERY_VARIANT),twrp)
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_DISABLE_TTF := true
 TW_EXCLUDE_SUPERSU := true
 TW_EXCLUDE_ENCRYPTED_BACKUPS := true
 TW_MAX_BRIGHTNESS := 255
 TW_NO_SCREEN_BLANK := true
+else
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
+endif
+
